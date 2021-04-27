844e2609a50f607600dbe6b655fc944e45d9cf9f
Puranjay Kwatra <55429956+puranjay123@users.noreply.github.com>
2021-04-27 19:31:39 +0530

    Delete LiveQuiz directory

--- LiveQuiz/src/Server/SqlQuery.java
+++
@@ -1,7 +0,0 @@
-package Server;
-
-public final class SqlQuery {
- public static final String DB_NAME = "project02";
- public static final String CREATE_TABLE_STRING = "CREATE TABLE questions (ID VARCHAR(30) PRIMARY KEY, qset JSON NOT NULL,time INTEGER NOT NULL)";
- public static final String CHECKID_STRING  = "SELECT ID FROM questions WHERE ID =?";
-}
